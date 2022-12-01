f4acc3732a7d65fc00379bf8bd10168524b1fe42
Narendra <Narendra@DESKTOP-C03GSGC>
2022-12-01 10:10:44 -0500

    Add project files.

---
+++ ClassLibary4Artifact/Algebra.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibary4Artifact
+{
+    public class Algebra
+    {
+        public double Addition(double x, double y)
+        {
+            return x + y;
+        }
+        public double Subtraction(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiplication(double x, double y)
+        {
+            return x * y;
+        }
+        public double Division(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
